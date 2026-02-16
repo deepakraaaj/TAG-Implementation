230dc287c4972c727c67bbe6c0829763c966c197
Deepakraj B <deepakraj.b@kritilabs.com>
2026-02-16 13:31:43 +0530

    Remove archived system_v1_clean folder

--- archived/system_v1_clean/app/core/logging.py
+++
@@ -1,6 +0,0 @@
-import logging
-from ..config import get_settings
-
-def setup_logging():
-    logging.basicConfig(level=get_settings().LOG_LEVEL)
-    logging.getLogger("uvicorn.access").disabled = False
